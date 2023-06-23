bc7a06e80f86d3c716972428389da1699534e997
medelmetoui <medelmetoui>
2023-06-23 17:18:57 +0100

    unscheduled

--- Force-unscheduled/__init__.py
+++ Force-unscheduled/__init__.py
@@ -15,15 +15,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     planning = json.dumps(body['planning'])
     demande = json.dumps(body['demande'])
 
-    ## First update planning
-    for trajet in planning:
-        for step in trajet:
-            if step['Magasin'] == demande['LIB_MAGASIN']:
-                if step['etat'] == "Dechargement":
-                    quantity_to_add = float(demande['DEMANDE'])
-                    current_qte = float(step['quantity'])
-                    current_qte = current_qte + quantity_to_add
-                    step['quantity'] = str(current_qte)
+    # ## First update planning
+    # for trajet in planning:
+    #     for step in trajet:
+    #         if step['Magasin'] == demande['LIB_MAGASIN']:
+    #             if step['etat'] == "Dechargement":
+    #                 quantity_to_add = float(demande['DEMANDE'])
+    #                 current_qte = float(step['quantity'])
+    #                 current_qte = current_qte + quantity_to_add
+    #                 step['quantity'] = str(current_qte)
     
     return func.HttpResponse(json.dumps(planning), mimetype="application/json")
 
