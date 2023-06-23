91b37808a76b0bef33d1ee5418c22542928d3301
medelmetoui <medelmetoui>
2023-06-23 17:43:43 +0100

    request to update unscheduled

--- Force-unscheduled/__init__.py
+++ Force-unscheduled/__init__.py
@@ -15,17 +15,21 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     planning = json.dumps(body['planning'])
     demande = json.dumps(body['demande'])
 
+    new_planning = json.loads(planning)
+    new_demande = json.loads(demande)
+
     # ## First update planning
-    # for trajet in planning:
-    #     for step in trajet:
-    #         if step['Magasin'] == demande['LIB_MAGASIN']:
-    #             if step['etat'] == "Dechargement":
-    #                 quantity_to_add = float(demande['DEMANDE'])
-    #                 current_qte = float(step['quantity'])
-    #                 current_qte = current_qte + quantity_to_add
-    #                 step['quantity'] = str(current_qte)
+    for trajet in new_planning:
+        for idx,step in enumerate(trajet['steps']):
+            if step['Magasin'] in new_demande['LIB_MAGASIN']:
+                if step['etat'] == "Dechargement":
+                    quantity_to_add = float(new_demande['DEMANDE'])
+                    current_qte = float(step['quantity'])
+                    current_qte = current_qte + quantity_to_add
+                    step['quantity'] = str(current_qte)
+                    
     
-    return func.HttpResponse(json.dumps(planning), mimetype="application/json")
+    return func.HttpResponse(json.dumps(new_planning), mimetype="application/json")
 
                 
 
