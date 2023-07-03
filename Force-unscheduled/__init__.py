8e40cdacf07cf530d13bae33a19ddef404d011a9
medelmetoui <medelmetoui>
2023-07-03 10:10:30 +0100

    force unscheduled update XUNSCHEDULED FILE

--- Force-unscheduled/__init__.py
+++ Force-unscheduled/__init__.py
@@ -44,6 +44,34 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 continue
     
     blob_client.upload_blob(json.dumps(new_planning),overwrite=True)
+
+    #### UPDATE THE UNSCHDULED FILE 
+
+    repository_name="SEC"
+
+    # active planning 
+    unscheduled_file_name = "XUNSCHEDULED_QUEUE.json"
+
+    ## FROM META WE GET THE ACTIVE PLANNING 
+    sas = generate_blob_sas(account_name="troblobstorage",
+                                container_name="output",
+                                blob_name=repository_name+"/"+unscheduled_file_name,
+                                account_key="YO16wuzFj6wkoK/AjMoYjEUcPXHWbkL1BmGc280AijovwovRP64DvMIY+e5i6b+m0BVJN1jdkNQ7+AStejBP9A==",
+                                permission=BlobSasPermissions(read=True,write=True,create=True),
+                                expiry=datetime.utcnow() + timedelta(hours=1))
+
+    url = "https://troblobstorage.blob.core.windows.net/output/"+repository_name+"/"+unscheduled_file_name+"?"+sas
+
+    blob_client = BlobClient.from_blob_url(url)
+    ## Compare new planning bel 9dim 
+    blob_data = blob_client.download_blob()
+    blob_content = blob_data.content_as_text()
+    unscheduled_file = json.loads(blob_content)
+
+    for demande in unscheduled_file:
+        if demande == new_demande:
+            unscheduled_file.remove(demande)
+            blob_client.upload_blob(json.dumps(unscheduled_file),overwrite=True)
                     
     return func.HttpResponse(json.dumps(new_planning), mimetype="application/json")
 
